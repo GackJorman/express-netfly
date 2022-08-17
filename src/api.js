3e8a78428af07abcd493f3c20d169cc0b8b84e9e
Jack Gorman <270101881@yoobeestudent.ac.nz>
2022-08-17 14:42:49 +1200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -8,5 +8,22 @@ router.get("/", (req, res) => {
     res.send("User List")
 })
 
+router.post("/", (req, res) => {
+    res.send("Create New User")
+})
+
+router
+.route("/:id")
+.get((req, res) => {
+    res.send(`Get a User With ID: ${req.params.id}`)
+})
+.put((req, res) => {
+    res.send(`Update a User With ID: ${req.params.id}`)
+})
+.delete((req, res) => {
+    res.send(`Delete a User With ID: ${req.params.id}`)
+})
+
+
 app.use("/.netlify/functions/api", router)
 module.exports.handler = serverless(app)
