92d285b5086bc0449a344167beaf7a46c2d76bd0
Jack Gorman <270101881@yoobeestudent.ac.nz>
2022-08-18 14:01:27 +1200

    body parser

--- src/api.js
+++ src/api.js
@@ -14,40 +14,40 @@ router.get("/", (req, res) => {
 })
 router.post("/", (req, res) => {
     // res.send("Create New User")
-     collection.insertOne(req.body, (err, res) => {
-         if (err) throw err;
-     })
+    collection.insertOne(req.body, (err, res) => {
+        if (err) throw err;
+    })
     res.send("1 document inserted.")
- })
- router
-     .route("/:id")
-     .get((req, res) => {
+})
+router
+    .route("/:id")
+    .get((req, res) => {
         //res.send(`Get a User with ID: ${req.params.id}`)
         const query = {
-             _id: ObjectId(req.params.id)
+            _id: ObjectId(req.params.id)
         }
         collection.findOne(query, (err, result) => {
-             if (err) throw err;
-             res.send(result)
+            if (err) throw err;
+            res.send(result)
         })
-     })
-     .put((req, res) => {
-         //res.send(`Update a User with ID: ${req.params.id}`)
-         const query = { _id: ObjectId(req.params.id) }
-         let newvalues = { $set: req.body };
-         collection.updateOne(query, newvalues, (err, result) => {
-             if (err) throw err;
-         })
-         res.send("1 document updated.")
-     })
-     .delete((req, res) => {
-         //res.send(`Delete a User with ID: ${req.params.id}`)
-         const query = { _id: ObjectId(req.params.id) }
-         collection.deleteOne(query, (err, result) => {
-             if (err) throw err;
-         })
-         res.send("1 document deleted.")
-     })
+    })
+    .put((req, res) => {
+        //res.send(`Update a User with ID: ${req.params.id}`)
+        const query = { _id: ObjectId(req.params.id) }
+        let newvalues = { $set: req.body };
+        collection.updateOne(query, newvalues, (err, result) => {
+            if (err) throw err;
+        })
+        res.send("1 document updated.")
+    })
+    .delete((req, res) => {
+        //res.send(`Delete a User with ID: ${req.params.id}`)
+        const query = { _id: ObjectId(req.params.id) }
+        collection.deleteOne(query, (err, result) => {
+            if (err) throw err;
+        })
+        res.send("1 document deleted.")
+    })
 
 app.use("/.netlify/functions/api", router)
 module.exports.handler = serverless(app)
